3b1deb9c3111e679831083c7268f3781e30b8edf
zukui <71074389+zukui1984@users.noreply.github.com>
2020-12-02 22:58:37 +0100

    Update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-let topBooks = [
+let topMovies = [
   {
     title: 'Harry Potter and the Sorcerer\'s Stone',
     author: 'J.K. Rowling'
@@ -18,61 +18,31 @@ let topBooks = [
 
 // GET requests
 app.get('/', (req, res) => {
-  res.send('Welcome to my book club!');
+  res.send('Welcome to my movie club!');
 });
 
 app.get('/documentation', (req, res) => {                  
   res.sendFile('public/documentation.html', { root: __dirname });
 });
 
-app.get('/books', (req, res) => {
-  res.json(topBooks);
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
 });
 
-// Middleware
+app.use(express.static('public'));
 
-let myLogger = (req, res, next) => {
-    console.log(req.url);
-    next();
-  };
-  
-  let requestTime = (req, res, next) => {
-    req.requestTime = Date.now();
-    next();
-  };
-  
-  app.use(myLogger);
-  app.use(requestTime);
-  
-  app.get('/', (req, res) => {
-    let responseText = 'Welcome to my app!';
-    responseText += '<small>Requested at: ' + req.requestTime + '</small>';
-    res.send(responseText);
-  });
-  
-  app.get('/secretUrl', (req, res) => {
-    let responseText = 'This is a secret url with super top-secret content.';
-    responseText += '<small>Requested at: ' + req.requestTime + '</small>';
-    res.send(responseText);
-  });
+let topMovies = (req, res, next) => {
+  console.log(req.url);
+  next();
+};
 
-    // Error Handling
-morgan = require('morgan');
-    const bodyParser = require('body-parser'),
-  methodOverride = require('method-override');
+let requestTime = (req, res, next) => {
+  req.requestTime = Date.now();
+  next();
+};
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-app.use(bodyParser.json());
-app.use(methodOverride());
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-
+app.use(topMovies);
+app.use(requestTime);
 
 // listen for requests
 app.listen(8080, () =>{
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ http
     if (q.pathname.includes("documentation")) {
       filePath = __dirname + "/documentation.html";
     } else {
-      filePath = "index.html";
+      filePath = "index.js";
     }
 
     fs.readFile(filePath, (err, data) => {
