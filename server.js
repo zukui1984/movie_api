d264b3765ccfd6c28c56fceb5ee6217c4d818ce7
zukui <71074389+zukui1984@users.noreply.github.com>
2020-12-02 09:26:38 +0100

    Update json

--- server.js
+++ server.js
@@ -1,36 +1,40 @@
-const http = require('http'),
-  fs = require('fs'),
-  url = require('url');
+const http = require("http"),
+  fs = require("fs"),
+  url = require("url");
 
-http.createServer((request, response) => {
-  let addr = request.url,
-    q = url.parse(addr, true),
-    filePath = '';
+http
+  .createServer((request, response) => {
+    let addr = request.url,
+      q = url.parse(addr, true),
+      filePath = "";
 
-  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
-    if (err) {
-      console.log(err);
+    fs.appendFile(
+      "log.txt",
+      "URL: " + addr + "\nTimestamp: " + new Date() + "\n\n",
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Added to log.");
+        }
+      }
+    );
+
+    if (q.pathname.includes("document")) {
+      filePath = __dirname + "/document.html";
     } else {
-      console.log('Added to log.');
-    }
-  });
-
-  if (q.pathname.includes('document')) {
-    filePath = (__dirname + '/document.html');
-  } else {
-    filePath = 'index.html';
-  }
-
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      throw err;
+      filePath = "index.html";
     }
 
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write(data);
-    response.end();
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        throw err;
+      }
 
-  });
-
-}).listen(8080);
-console.log('My test server is running on Port 8080.');
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(data);
+      response.end();
+    });
+  })
+  .listen(8080);
+console.log("My test server is running on Port 8080.");
