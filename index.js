91774762f5ef5c0aa62af4eeb5db6603ff2bbcb0
zukui <71074389+zukui1984@users.noreply.github.com>
2020-12-04 21:50:36 +0100

    Update

--- index.js
+++ index.js
@@ -1,41 +1,93 @@
-const express = require('express');
+const express = require("express");
+(bodyParser = require("body-parser")), (uuid = require("uuid"));
+morgan = require("morgan");
+
 const app = express();
-morgan = require('morgan');
+
+app.use(bodyParser.json());
 
 let topMovies = [
   {
-    title: 'Harry Potter and the Sorcerer\'s Stone',
-    author: 'J.K. Rowling'
+    id: 1,
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: "J.K. Rowling",
   },
   {
-    title: 'Lord of the Rings',
-    author: 'J.R.R. Tolkien'
+    id: 2,
+    title: "Lord of the Rings",
+    author: "J.R.R. Tolkien",
   },
   {
-    title: 'Twilight',
-    author: 'Stephanie Meyer'
-  }
+    id: 3,
+    title: "Twilight",
+    author: "Stephanie Meyer",
+  },
 ];
 
-app.use(express.static('public'));
-app.use(morgan('public'));
+app.use(express.static("public"));
+app.use(morgan("public"));
 
 // GET requests
-app.get('/', (req, res) => {
-  res.send('Welcome to my movie club!');
+app.get("/", (req, res) => {
+  res.send("Successful GET request returning data on all movies");
 });
 
-app.get('/documentation', (req, res) => {                  
-  res.sendFile('public/documentation.html', { root: __dirname });
+app.get("/documentation", (req, res) => {
+  res.sendFile("public/documentation.html", { root: __dirname });
 });
 
-app.get('/movies', (req, res) => {
+app.get("/movies", (req, res) => {
   res.json(topMovies);
 });
 
+// POST
+app.post("/movies", (req, res) => {
+  let topMovies = req.body;
 
+  if (!topMovies.title) {
+    const title = 'Missing "title" in request body';
+    res.status(400).send(title);
+  } else {
+    topMovies.id = uuid.v4();
+    movies.push(topMovies);
+    res.status(201).send(topMovies);
+  }
+});
+
+// Request Parameters
+app.get("/movies/:title", (req, res) => {
+  res.json(
+    movies.find((movie) => {
+      return movies.title === req.params.title;
+    })
+  );
+});
+
+// Responses & Status Codes
+app.put("/movies/:id/:title/:author", (req, res) => {
+  let movie = movies.find((id) => {
+    return movie.id === req.params.id;
+  });
+
+  if (movie) {
+    movie.title[req.params.title] = parseInt(req.params.author);
+    res
+      .status(201)
+      .send(
+        req.params.id +
+          "Movie " +
+          req.params.title +
+          "directed by " +
+          req.params.author
+      );
+  } else {
+    res
+      .status(404)
+      .send("Movie with the name " + req.params.id + " was not found.");
+  }
+});
 
 // listen for requests
-app.listen(8080, () =>{
-  console.log('Your app is listening on port 8080.');
+app.listen(8080, () => {
+  console.log("Your app is listening on port 8080.");
 });
