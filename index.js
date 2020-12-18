9193664f2360a3e7c0e2d8b44656634c0fd38514
zukui <71074389+zukui1984@users.noreply.github.com>
2020-12-18 22:58:42 +0100

    update

--- index.js
+++ index.js
@@ -1,170 +1,79 @@
 const express = require("express");
-bodyParser = require("body-parser");
-uuid = require("uuid");
-morgan = require("morgan");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
 const Models = require("./models.js");
 
 const Movies = Models.Movie;
 const Users = Models.User;
-const Genres = Models.Genre;
-const Directors = Models.Director;
 
 // To connect Mongoose
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 
-app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use(morgan("public"));
+app.use(bodyParser.json());
 
-let movies = [
-  {
-    id: 1,
-    title: "Batman",
-    director: "Christopher Nolan",
-    genre: "Action"
-  },
+// Auth.js - login authentication
+let auth = require('./auth')(app);
 
-  {
-    id: 2,
-    title: "Godfather",
-    director: "Martin Scorsese",
-    genres: "Crime"
-  },
-
-  {
-    id: 3,
-    title: "Avatar",
-    director: "James Cameron",
-    genre: "Fantasy"
-  },
-];
-
-let genre = [
-  {
-    Name: "Fantasy",
-    Description:
-      "Films that belong to the fantasy genre with fantastic themes, usually magic, supernatural events, mythology, folklore, or exotic fantasy worlds.",
-  },
-
-  {
-    Name: "Crime",
-    Description:
-      "A film genre inspired by and analogous to the crime fiction literary genre.",
-  },
-
-  {
-    Name: "Action",
-    Description:
-      "A film genre in which the protagonist or protagonists are thrust into a series of events that typically violence, physical feats, rescues and frantic chases.",
-  },
-];
-
-let directors = [
-  {
-    name: "Martin Scorsese",
-    bio: "An American director, producer, and screenwriter.",
-    birthday: "1942-11-17",
-  },
-
-  {
-    name: "James Cameron",
-    bio: "An American producer for movies Titanic and Avatar",
-    birthday: "1954-08-16",
-  },
-
-  {
-    Name: "Christopher Nolan",
-    Bio: "Famous director who directed Batman trilogy movies.",
-    Birthday: "1970-07-30",
-  },
-];
-
-let users = [[]];
-
-// error handling
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
-});
+const passport = require("passport");
+require("./passport");
 
 // GET requests
 app.get("/", (req, res) => {
   res.send("Successful GET request returning data on all movies");
 });
 
-app.get("/documentation", (req, res) => {
-  res.sendFile("public/documentation.html", { root: __dirname });
-});
+// app.get("/documentation", (req, res) => {
+//   res.sendFile("public/documentation.html", { root: __dirname });
+// });
 
-//Return all movies
-app.get('/movies', (req, res) => {
+// JWT PASSPORT AUTHENTICATION
+app.get("/movies", passport.authenticate("jwt", { session: false }), (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
     })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Error: " + error);
     });
 });
 
-//GET JSON - Specific files - A
-app.get("/movies/:title/", (req, res) => {
-Movies.findOne({ title: req.params.title})
-.then((movie) => {
-  res.json(movie);
-})
-.catch((err) => {
-  console.error(err);
-  res.status(500).send("Error: " + err);
-});
+// JWT Movie - TITLE
+app.get("/movies/:title", passport.authenticate("jwt", { session: false }), (req, res) => {
+  Movies.findOne( {title: req.params.title})
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Error: " + error);
+    });
 });
 
-//GET JSON - Specific files - B
-app.get("/genre/:name/", (req, res) => {
-  Movies.findOne({ title: req.params.title})
-  .then((genre) => {
-    res.json(genre);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Error: " + err);
-  });
-  });
-
-//GET JSON - Specific files - C
-app.get("/director/:name/", (req, res) => {
-  Movies.findOne({ title: req.params.title})
-  .then((director) => {
-    res.json(director);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Error: " + err);
-  });
-  });
 
 // Get all USERS
-app.get('/users', (req, res) => {
+app.get("/users", passport.authenticate("jwt", { session: false }), (req, res) => {
   Users.find()
     .then((users) => {
       res.status(201).json(users);
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send('Error: ' + err);
+      res.status(500).send("Error: " + err);
     });
 });
 
+
+
 // PUT - findOne and Update 
-app.put('/users/:Username', (req, res) => {
-  Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
+app.put("/users/:Username", passport.authenticate("jwt", { session: false }), (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, 
+  { $set:
     {
       Username: req.body.Username,
       Password: req.body.Password,
@@ -176,7 +85,7 @@ app.put('/users/:Username', (req, res) => {
   (err, updatedUser) => {
     if(err) {
       console.error(err);
-      res.status(500).send('Error: ' + err);
+      res.status(500).send("Error: " + err);
     } else {
       res.json(updatedUser);
     }
@@ -187,7 +96,7 @@ app.put('/users/:Username', (req, res) => {
 // POST
 
 //User to REGISTER
-app.post("/users", (req, res) => {
+app.post("/users", passport.authenticate('jwt', { session: false }), (req, res) => {
   Users.findOne({ Username: req.body.Username })
     .then((user) => {
       if (user) {
@@ -208,28 +117,30 @@ app.post("/users", (req, res) => {
           });
       }
     })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send("Error: " + error);
-    });
 });
 
 // DELETE
-app.delete('/users/:username', (req, res) => {
+app.delete("/users/:username", passport.authenticate("jwt", { session: false }), (req, res) => {
   Users.findOneAndRemove({ Username: req.params.username })
     .then((user) => {
       if (!user) {
-        res.status(400).send(req.params.Username + ' was not found');
+        res.status(400).send(req.params.Username + " was not found");
       } else {
-        res.status(200).send(req.params.Username + ' was deleted.');
+        res.status(200).send(req.params.Username + " was deleted.");
       }
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send('Error: ' + err);
+      res.status(500).send("Error: " + err);
     });
 });
 
+
+// // error handling
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).send("Something broke!");
+// });
 
 
 // listen for requests
