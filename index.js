90e7ee5f9bfcc82fa6a3f784038581d515e5949f
zukui <71074389+zukui1984@users.noreply.github.com>
2020-12-21 12:56:39 +0100

    commit name

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 // To connect Mongoose
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 
